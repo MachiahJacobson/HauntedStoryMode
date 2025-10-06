a15b072890362d396ede5bd73572e42eeec5ab1e
Mac <macjacobson99@gmail.com>
2025-10-06 13:33:02 -0600

    Initial Commit

---
+++ HauntedStoryMode/HauntedStoryModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HauntedStoryModeApp.swift
+//  HauntedStoryMode
+//
+//  Created by Jacobson, Machiah - Student on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HauntedStoryModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HauntedStoryModeTests/HauntedStoryModeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HauntedStoryModeTests.swift
+//  HauntedStoryModeTests
+//
+//  Created by Jacobson, Machiah - Student on 10/6/25.
+//
+
+import Testing
+@testable import HauntedStoryMode
+
+struct HauntedStoryModeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
